a7ca0c193b17ca4c722c818f66ce625035d20ece
Burhan <burhan221b@gmail.com>
2020-07-26 21:15:22 -0700

    Created reverseInt algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { reverseString, isPalindrome } from "./session_1/index";
+import { reverseString, isPalindrome, reverseInt } from "./session_1/index";
 
 console.log("Hello World");
 
@@ -7,3 +7,6 @@ console.log(reverseString("hello")); // olleh
 console.log(isPalindrome("racecar")); // true
 console.log(isPalindrome("ra ce  car")); // true
 console.log(isPalindrome("hello world")); // false
+
+console.log(reverseInt(221)); // 122
+console.log(reverseInt(521)); // 125
--- src/session_1/index.ts
+++ src/session_1/index.ts
@@ -1,6 +1,7 @@
 // Include all the algorithms from session_1 folder and import here
 import reverseString from "./reverseString";
 import isPalindrome from "./isPalindrome";
+import reverseInt from "./reverseInt";
 
 // Include all the algorithms from imported from session_1 folder and export here
-export { reverseString, isPalindrome };
+export { reverseString, isPalindrome, reverseInt };
